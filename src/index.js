efdbc03fede4f90dd5d7365af9d589052e1fed5a
Sebastian Claros <sclaros@salesforce.com>
2021-04-22 13:09:16 -0300

    track

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import MyPricing from  'pricing/view';
 
 customElements.define('pricing-view', MyPricing.CustomElementConstructor);
 
+
+const destroy = () => {
+    let element = document.getElementsByTagName("pricing-view")[0];
+    element.parentElement.removeChild(element);
+};
+const change = () => {
+    let element = document.getElementsByTagName("pricing-view")[0];
+    element.name = (new Date()).toString();
+};
+
+document.getElementById('destroy').addEventListener( "click", destroy);
+document.getElementById('change').addEventListener( "click", change);
+
 export { createElement, MyPricing };
