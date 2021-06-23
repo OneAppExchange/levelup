c776bc0cadd3c5bff8d4e0fe69f52e1b1cf8ab4b
Sebastian Claros <sclaros@salesforce.com>
2021-06-23 20:39:47 -0300

    purge

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import appxTheme from './theme.js';
-
-addons.setConfig({
-    theme: appxTheme
-});
--- .storybook/theme.js
+++
@@ -1,8 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-    brandTitle: 'Appexchange Shared Library',
-    brandUrl: 'https://appx-shared.herokuapp.com',
-    brandImage:
-        'https://appexchange.salesforce.com/resource/1618441898000/assetsappx/images/logo-appexchange-2x.png'
-});
